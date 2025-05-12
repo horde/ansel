5ab40099bfd1c9682e06a9fba00c3394b04890e2
Ralf Lang <ralf.lang@ralf-lang.de>
2025-05-12 21:37:24 +0200

    fix: Make ansel use a classmap for lib as it's not strictly PSR-0

---
+++ src/Router.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace Horde\Ansel;
+
+/**
+ * Ansel Router
+ *
+ * @category Horde
+ * @package  Ansel
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+class Router
+{
+}
